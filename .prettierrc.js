e5db2247a24bda11e5f209922618830047f60377
Denis <wolf-den@tut.by>
2021-09-01 00:29:56 +0300

    init: start project pointing poker

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  useTabs: false,
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  tabWidth: 2
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './App.css';
+
+const App: FC = () => {
+    return (
+        <div className="App">
+            <p>Hello World!</p>
+        </div>
+    );
+};
+
+export default App;
