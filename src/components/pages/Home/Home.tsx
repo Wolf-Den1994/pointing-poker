0b9228ed18d2c9b2df105973ca3f0e6a8e139871
KalashnikovTV <max.malaschkov@mail.ru>
2021-09-03 16:22:50 +0300

    fix: remove main tag

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import cl from './Home.module.scss';
 
 const Home: React.FC = () => {
-  return <main className={cl.home}>Welcome screen</main>;
+  return <div className={cl.home}>Welcome screen</div>;
 };
 
 export default Home;
