42ebb61d4e4077966a82047b7b8bd33c0443d65d
KalashnikovTV <max.malaschkov@mail.ru>
2021-09-04 14:10:40 +0300

    feat: implement welcome screen

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import imagePokerPlanning from '../../assets/images/poker-planning.png';
 
-// import style from './Home.module.scss';
+import style from './Home.module.scss';
 
 const Home: React.FC = () => {
-  return <div>Welcome screen</div>;
+  return (
+    <div className={style.wrapper}>
+      <img className={style.image} src={imagePokerPlanning} alt="poker-planning" />
+      <div className={style.row}>
+        <h1 className={style.title}>Start your planning:</h1>
+        <div className={style.box}>
+          <p className={style.session}>Create a session: </p>
+          <button className={style.button} type="button" onClick={() => {}}>
+            Start new game
+          </button>
+        </div>
+      </div>
+
+      <div className={style.row}>
+        <h1 className={`${style.title} ${style.title_lobby}`}>OR:</h1>
+        <div className={`${style.box} ${style.box_lobby}`}>
+          <p className={style.session}>
+            Connect to lobby by <span>URL:</span>
+          </p>
+          <form className={style.lobby} onSubmit={(e) => e.preventDefault()}>
+            <input className={style.input} type="text" onChange={() => {}} />
+            <button className={`${style.button} ${style.button_lobby}`} type="submit">
+              Connect
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
