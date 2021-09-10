dcf56433f87d1236fa3304f62030a805ac16771c
Artsiom Murashko <ksarelto@tut.by>
2021-09-10 03:22:56 +0300

    feat: add socket api

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import { router } from './API/router';
+import {
+  getRoom, setRoom, updateRoom, addNewUser, deleteUser,
+} from './API/mongoDB';
+import { Room } from './types/types';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,10 +21,6 @@ const io = new Server(httpServer, {
     origin: '*',
     methods: ['GET', 'POST'],
   },
-});
-
-io.on('connection', () => {
-  console.log('kkk');
 });
 
 async function start() {
@@ -37,8 +39,81 @@ async function start() {
   }
 }
 
-app.use('/', (req, res) => {
-  res.send('Test');
+start();
+
+const room = {
+  roomId: '',
+  admin: {},
+  users: [],
+  messages: [],
+};
+
+io.on('connection', (socket) => {
+  socket.on('createRoom', async ({ data }) => {
+    const roomId = uuidv4();
+    socket.join(roomId);
+    const user = { ...data, id: socket.id, role: 'admin' };
+    const newUserRoom = {
+      ...room,
+      roomId,
+      admin: data,
+      users: [user],
+    };
+    await setRoom(newUserRoom as unknown as Room);
+    socket.emit('returnRoomId', { id: roomId, user });
+  });
+
+  socket.on('enterRoom', ({ user, roomId }) => {
+    socket.join(roomId);
+    const newUser = { ...user, id: socket.id };
+    addNewUser(roomId, newUser);
+    socket.broadcast.to(roomId).emit('enteredRoom', { user: newUser });
+  });
+
+  socket.on('getMessage', async ({ roomId, user, mess }) => {
+    const searchingRoom = await getRoom(roomId);
+    searchingRoom.messages.push({ name: user, message: mess });
+    await updateRoom(searchingRoom);
+    socket.broadcast.to(roomId).emit('sendMessage', { name: user, message: mess });
+  });
+
+  socket.on('someOneWriteMessage', ({ user, write, roomId }) => {
+    io.to(roomId).emit('sendMessageWriter', { name: user, active: write });
+  });
+
+  socket.once('disconnectAll', async ({ roomId }) => {
+    io.in(roomId).emit('dissconnectAllSockets');
+    io.in(roomId).disconnectSockets();
+  });
+
+  socket.on('disconnectOne', async ({ userId, roomId }) => {
+    const sockets = await io.in(roomId).fetchSockets();
+    sockets.forEach((el) => {
+      if (el.id === userId) {
+        io.to(roomId).emit('sendUserDisconnected', `${userId} is disconnected`);
+        el.emit('willBeDisconnected');
+        deleteUser(roomId, el.id);
+        el.leave(roomId);
+        el.disconnect(true);
+      }
+    });
+  });
+  socket.on('setTimeOnTimer', ({ time, roomId }) => {
+    io.in(roomId).emit('sendTimeOnTimer', time);
+  });
+
+  socket.once('leaveRoom', ({ roomId, user, id }) => {
+    deleteUser(roomId, id);
+    socket.broadcast.in(roomId).emit('userLeaveTheRoom', { user, userId: id });
+    socket.leave(roomId);
+    socket.disconnect(true);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('disconnected');
+  });
+
+  console.log('socket connected', socket.id);
 });
 
-start();
+app.use('/api', router);
