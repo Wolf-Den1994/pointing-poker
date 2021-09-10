ddb2941512a4dbe498094128b24f6c294ae6bc44
Artsiom Murashko <ksarelto@tut.by>
2021-09-10 03:21:19 +0300

    feat: create mogo model

---
+++ src/models/models.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+export const RoomModel = new Schema({
+  roomId: { type: String },
+  admin: {
+    id: String,
+    name: String,
+    lastName: String,
+    position: String,
+    role: String,
+    avatarUrl: String,
+  },
+  users: [
+    {
+      id: String,
+      name: String,
+      lastName: String,
+      position: String,
+      role: String,
+      avatarUrl: String,
+    },
+  ],
+  messages: [{ name: String, message: String }],
+});
