b255548196b6f91de6deed2793fc96d93c6621e2
Artsiom Murashko <ksarelto@tut.by>
2021-09-15 19:34:21 +0300

    refactor: change socket link

--- src/utils/soketIO.ts
+++ src/utils/soketIO.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://rsschool-pp.herokuapp.com/');
+const socket = io('https:/https://rsschool-pp.herokuapp.com');
 
 export default socket;
