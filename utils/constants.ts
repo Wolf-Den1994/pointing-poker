4d95bb4572d3eaa3733e48ef7581dc47157aeb48
Artsiom Murashko <ksarelto@tut.by>
2021-09-17 00:22:53 +0300

    feat: create folder utils with file constants, add const room and clearVoting

---
+++ utils/constants.ts
@@ -0,0 +1,18 @@
+export const room = {
+  roomId: '',
+  admin: {},
+  users: [],
+  messages: [],
+  issues: [],
+  voting: {
+    id: '',
+    voices: 0,
+    votedUsers: 0,
+  },
+};
+
+export const clearVoting = {
+  id: '',
+  voices: 0,
+  votedUsers: 0,
+};
