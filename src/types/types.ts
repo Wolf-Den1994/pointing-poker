2450f01c9ab948039d1a7dff9b3cb6fd1dfa1c64
Artsiom Murashko <ksarelto@tut.by>
2021-09-17 00:21:04 +0300

    feat: add issues and voting to Room interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -7,6 +7,8 @@ export interface Room {
     id: string, name: string, lastName: string, position: string, role: string, avatarUrl: string,
   }],
   messages: [{ name: string, message: string }],
+  issues: [string];
+  voting: { id: string; voices: number; votedUsers: number };
 }
 
 export interface User {
