94ad9317f975598c40e4ceff37b16ae01c7c90a8
Artsiom Murashko <ksarelto@tut.by>
2021-09-17 00:28:00 +0300

    feat: create func clearVotingObj, add import of cclearVoting

--- src/API/mongoDB.ts
+++ src/API/mongoDB.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
-import { RoomModel } from '../models/models.js';
-import { Room, User } from '../types/types.js';
+import { RoomModel } from '../models/models';
+import { Room, User } from '../types/types';
+import { clearVoting } from '../utils/constants';
 
 const RoomMongo = mongoose.model('RoomMongo', RoomModel, 'pp-database');
 
@@ -16,6 +17,17 @@ export const getRoom = async (id: string): Promise<Room> => {
 
 export const updateRoom = async (room: Room): Promise<void> => {
   await RoomMongo.findOneAndUpdate({ roomId: room.roomId }, room);
+  return Promise.resolve();
+};
+
+export const clearVotingObj = async (roomId: string, id = ''): Promise<void> => {
+  const response = await RoomMongo.findOne({ roomId });
+  if (id !== '') {
+    const newUsersArray = await response.users.filter((el: User) => el.id !== id);
+    response.users = newUsersArray;
+  }
+  response.voting = clearVoting;
+  await RoomMongo.findOneAndUpdate({ roomId: response.roomId }, response);
   return Promise.resolve();
 };
 
