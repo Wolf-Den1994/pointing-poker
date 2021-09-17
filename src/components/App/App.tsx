2f9448234c8218f52822f388300a1f8b44b9e3fa
Denis <wolf-den@tut.by>
2021-09-17 11:27:29 +0300

    fix: remove location in Switch

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Switch, Redirect, useLocation } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import style from './App.module.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
@@ -6,14 +6,12 @@ import Home from '../../pages/Home/Home';
 import Lobby from '../../pages/Lobby/Lobby';
 
 const App: React.FC = () => {
-  const location = useLocation();
-
   return (
     <div className={style.wrapper}>
       <Header />
       <main>
         <div className="container">
-          <Switch location={location}>
+          <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/lobby/:roomId" component={Lobby} />
             <Route path="*" render={() => <Redirect to="/" />} />
