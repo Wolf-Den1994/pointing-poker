ddc0027ba33f67d66630cc755ec06a20cd0893e4
Artsiom Murashko <ksarelto@tut.by>
2021-09-20 22:09:48 +0300

    feat: add const settings, time, roomId, fill func handleCloseModal with methods

--- src/components/GameSettingsPopup/GameSettingsPopup.tsx
+++ src/components/GameSettingsPopup/GameSettingsPopup.tsx
@@ -1,14 +1,25 @@
 import { SettingOutlined } from '@ant-design/icons';
 import { Button, Modal } from 'antd';
 import { useState } from 'react';
+import { useParams } from 'react-router';
 import GameSettings from '../GameSettings/GameSettings';
+import useTypedSelector from '../../hooks/useTypedSelector';
+import { SocketTokens } from '../../types/types';
+import { emit } from '../../services/socket';
 
 const GameSettingsPopup: React.FC = () => {
   const [modalActive, setModalActive] = useState(false);
+  const { settings } = useTypedSelector((state) => state.settings);
+  const { time } = useTypedSelector((state) => state.timer);
+  const { roomId } = useParams<{ roomId: string }>();
 
   const handleOpenModal = () => setModalActive(true);
 
-  const handleCloseModal = () => setModalActive(false);
+  const handleCloseModal = () => {
+    const newSettings = { ...settings, roundTime: settings.roundTime.format() };
+    emit(SocketTokens.SendNewSettingsToUsers, { roomId, settings: newSettings, time });
+    setModalActive(false);
+  };
 
   return (
     <>
