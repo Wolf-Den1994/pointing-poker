384e218362ae05a45ed606492953a995c95e0467
KalashnikovTV <max.malaschkov@mail.ru>
2021-09-21 20:07:26 +0300

    fix: add animation for svg, create component for svg

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 import style from './NotFound.module.scss';
-import imageNotFound from '../../assets/404.svg';
 import { PathRoutes, TextForUser } from '../../types/types';
+import NotFoundSvg from '../../components/NotFoundSvg/NotFoundSvg';
 
 const NotFound: React.FC = () => {
   return (
     <div className={style.error}>
       <div className={style.wrapper}>
-        <img src={imageNotFound} alt="404-error" />
+        <NotFoundSvg />
         <div className={style.page}>{TextForUser.PageNotFound}</div>
         <Link to={PathRoutes.Home} className={style.back}>
           {TextForUser.GoBack}
