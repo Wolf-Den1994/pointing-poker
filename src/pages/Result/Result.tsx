402bed78322ae303650773d53618998af759d797
Denis <wolf-den@tut.by>
2021-09-23 11:38:50 +0300

    init: start task export game result file'

---
+++ src/pages/Result/Result.tsx
@@ -0,0 +1,11 @@
+import style from './Result.module.scss';
+
+const Result: React.FC = () => {
+  return (
+    <div className={style.result}>
+      <div>Result Game</div>
+    </div>
+  );
+};
+
+export default Result;
