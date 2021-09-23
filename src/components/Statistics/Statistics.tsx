eac1f4850e022c72cd01fb4affc8fd1be61b4603
Artsiom Murashko <ksarelto@tut.by>
2021-09-24 01:51:44 +0300

    feat: change statistic type, statisticReducer, change Statistic component

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -2,19 +2,28 @@ import useTypedSelector from '../../hooks/useTypedSelector';
 import GameCard from '../GameCard/GameCard';
 import style from './Statistics.module.scss';
 
-const Statistics: React.FC = () => {
+interface IStatisticProps {
+  activeIssue: string;
+}
+
+const Statistics: React.FC<IStatisticProps> = ({ activeIssue }: IStatisticProps) => {
   const { statistics } = useTypedSelector((state) => state.statistics);
+
+  const findNeededStatistic = (taskName: string) => {
+    const result = statistics.find((el) => el.taskName === taskName);
+    return result;
+  };
 
   return (
     <div className={style.statistics}>
       <p className={style.title}>Statistics:</p>
       <div className={style.wrapper}>
-        {statistics.map((item) => (
+        {findNeededStatistic(activeIssue)?.statisticValues.map((item) => (
           <span key={item.card}>
             <div className={style.card}>
               <GameCard small>{item.card}</GameCard>
             </div>
-            <div className={style.percent}>{item.averageGrade}</div>
+            <div className={style.percent}>{item.averageValue}</div>
           </span>
         ))}
       </div>
