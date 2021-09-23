ece47103167c147cebcc208355caa9f65ad37f4d
Artsiom Murashko <ksarelto@tut.by>
2021-09-24 01:32:45 +0300

    refactor: change some params for better display of statistic

--- src/components/ExportFile/ExportFile.tsx
+++ src/components/ExportFile/ExportFile.tsx
@@ -4,15 +4,29 @@ import useTypedSelector from '../../hooks/useTypedSelector';
 import style from './ExportFile.module.scss';
 
 const filename = 'result-game-pointing-poker';
+interface SavedIssuesList {
+  issue?: string;
+  card?: string;
+  averageValue?: string;
+}
 
 const ExportFile: React.FC = () => {
   const { statistics } = useTypedSelector((state) => state.statistics);
-
+  const resultArrs = () => {
+    const result: SavedIssuesList[] = [];
+    statistics.forEach((el) => {
+      el.statisticValues.forEach((elem) => {
+        result.push({ issue: el.taskName, card: elem.card, averageValue: elem.averageValue });
+      });
+    });
+    console.log(result);
+    return result;
+  };
   return (
     <div className={style.exportFile}>
       <span>Download the result of the game in format:</span>
-      <ExportXLSX xlsxData={statistics} file={filename} />
-      <ExportCSV csvData={statistics} filename={filename} />
+      <ExportXLSX xlsxData={resultArrs()} file={filename} />
+      <ExportCSV csvData={resultArrs()} filename={filename} />
     </div>
   );
 };
