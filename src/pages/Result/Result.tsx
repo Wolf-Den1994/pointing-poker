2c83c25f74de97b2211fcb630ce7aba81f5233a5
Denis <wolf-den@tut.by>
2021-09-23 18:10:20 +0300

    fix: remove div plug

--- src/pages/Result/Result.tsx
+++ src/pages/Result/Result.tsx
@@ -4,7 +4,6 @@ import style from './Result.module.scss';
 const Result: React.FC = () => {
   return (
     <div className={style.result}>
-      <div>Result Game</div>
       <ExportFile />
     </div>
   );
