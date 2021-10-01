0fa140b4bac8d13a3bb9d9d17ea0dca9e20eb5c3
Artsiom Murashko <ksarelto@tut.by>
2021-10-01 18:48:04 +0300

    feat: implement disconnectUsers func

---
+++ src/utils/disconnectUsers.ts
@@ -0,0 +1,15 @@
+import { deleteRoom } from '../services/api';
+import { emit } from '../services/socket';
+import { SocketTokens } from '../types/types';
+import socket from './soketIO';
+
+const disconnectUsers = async (roomId: string, isDealer: boolean, userName?: string): Promise<void> => {
+  if (isDealer) {
+    emit(SocketTokens.DisconnectAll, { roomId });
+    await deleteRoom({ data: { id: roomId } });
+  } else {
+    emit(SocketTokens.LeaveRoom, { roomId, user: userName, id: socket.id });
+  }
+};
+
+export default disconnectUsers;
