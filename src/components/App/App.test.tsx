62c048b9711fd366fc58bff719f47622ea29e92b
Maksim Malashkou <max.malaschkov@mail.ru>
2021-10-03 22:06:46 +0300

    feat: implement app renders without crashing test

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import store from '../../store/store';
+import App from './App';
+
+it('App renders without crashing', () => {
+  render(
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>,
+  );
+
+  const root = document.createElement('div');
+  ReactDOM.render(
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>,
+    root,
+  );
+  ReactDOM.unmountComponentAtNode(root);
+});
