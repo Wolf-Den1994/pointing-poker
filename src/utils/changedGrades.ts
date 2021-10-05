2ffcf5dbb41a8b0980ed8c389282158e9c2670eb
Artsiom Murashko <ksarelto@tut.by>
2021-10-05 13:02:13 +0300

    refactor: update changeGrades func

--- src/utils/changedGrades.ts
+++ src/utils/changedGrades.ts
@@ -1,12 +1,11 @@
 import { IIssueGrade } from '../types/types';
 
 export const changeGrades = (grades: IIssueGrade[], newGrade: IIssueGrade): IIssueGrade[] => {
-  const newGradesArray = [...grades];
-  const existGrade = newGradesArray.find((grade) => grade.name === newGrade.name);
+  const existGrade = grades.find((grade) => grade.name === newGrade.name);
   return existGrade
-    ? newGradesArray.map((grade) => {
+    ? grades.map((grade) => {
         if (grade.name === newGrade.name) return { ...grade, grade: newGrade.grade };
         return grade;
       })
-    : [...newGradesArray, newGrade];
+    : [...grades, newGrade];
 };
