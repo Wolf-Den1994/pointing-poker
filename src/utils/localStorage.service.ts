cf3bfdda5bc938c27c350f7e72edf2c86bec676e
Artsiom Murashko <ksarelto@tut.by>
2021-10-06 16:02:08 +0300

    refactor: update localStorage.service file

--- src/utils/localStorage.service.ts
+++ src/utils/localStorage.service.ts
@@ -1,49 +1,48 @@
 import { ICardData, ICardSetsData } from '../types/types';
 
-export const createLocalStorage = (roomId: string, data: ICardSetsData): void => {
-  window.localStorage.setItem(roomId, JSON.stringify(data));
+const getCardSetstLocalStorage = (roomId: string): ICardSetsData | null => {
+  const cardSets = window.localStorage.getItem(roomId);
+  if (cardSets) {
+    return JSON.parse(cardSets);
+  }
+  return null;
 };
 
-export const deleteLocalStorage = (roomId: string): void => {
+export const setCardSetsLocalStorage = (roomId: string, data: ICardSetsData): void =>
+  window.localStorage.setItem(roomId, JSON.stringify(data));
+
+export const deleteCardSetsLocalStorage = (roomId: string): void => {
   window.localStorage.removeItem(roomId);
 };
 
-export const addCardToLocalStorage = (roomId: string, cardsName: string, newCard: ICardData): void => {
-  const cardSets = window.localStorage.getItem(roomId);
-  if (cardSets) {
-    const storage = JSON.parse(cardSets);
-    storage[cardsName].push(newCard);
-    window.localStorage.setItem(roomId, JSON.stringify(storage));
+export const addCardToLocalStorage = (roomId: string, cardName: string, newCard: ICardData): void => {
+  const storage = getCardSetstLocalStorage(roomId);
+  if (storage) {
+    storage[cardName].push(newCard);
+    setCardSetsLocalStorage(roomId, storage);
   }
 };
 
-export const editCardInLocalStorage = (roomId: string, cardsName: string, newCard: string, oldCard: string): void => {
-  const cardSets = window.localStorage.getItem(roomId);
-  if (cardSets) {
-    const storage = JSON.parse(cardSets);
-    storage[cardsName].forEach((card: ICardData) => {
+export const editCardInLocalStorage = (roomId: string, cardName: string, newCard: string, oldCard: string): void => {
+  const storage = getCardSetstLocalStorage(roomId);
+  if (storage) {
+    storage[cardName].forEach((card: ICardData) => {
       const editCard = card;
       if (card.card === oldCard) editCard.card = newCard;
     });
-    window.localStorage.setItem(roomId, JSON.stringify(storage));
+    setCardSetsLocalStorage(roomId, storage);
   }
 };
 
-export const deleteCardFromLocalStorage = (roomId: string, cardsName: string, deletedCard: string): void => {
-  const cardSets = window.localStorage.getItem(roomId);
-  if (cardSets) {
-    const storage = JSON.parse(cardSets);
-    const index = storage[cardsName].findIndex((card: ICardData) => card.card === deletedCard);
-    storage[cardsName].splice(index, 1);
-    window.localStorage.setItem(roomId, JSON.stringify(storage));
+export const deleteCardFromLocalStorage = (roomId: string, cardName: string, deletedCard: string): void => {
+  const storage = getCardSetstLocalStorage(roomId);
+  if (storage) {
+    storage[cardName] = storage[cardName].filter((card: ICardData) => card.card !== deletedCard);
+    setCardSetsLocalStorage(roomId, storage);
   }
 };
 
-export const getCardsFromLocalStorage = (roomId: string, cardsName: string): ICardData[] | [] => {
-  const cardSets = window.localStorage.getItem(roomId);
-  if (cardSets) {
-    const storage = JSON.parse(cardSets);
-    return storage[cardsName];
-  }
-  return [];
+export const getCardsFromLocalStorage = (roomId: string, cardName: string): ICardData[] | [] => {
+  const storage = getCardSetstLocalStorage(roomId);
+  return storage ? storage[cardName] : [];
 };
