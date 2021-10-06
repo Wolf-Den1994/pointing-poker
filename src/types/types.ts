7e5a80302d4a23b43c0b7bdab94099bc3ec20c48
Artsiom Murashko <ksarelto@tut.by>
2021-10-06 16:01:01 +0300

    refactor: change ICardStsData type

--- src/types/types.ts
+++ src/types/types.ts
@@ -226,7 +226,7 @@ export interface ICardData {
 }
 
 export interface ICardSetsData {
-  fibonacci: ICardData[];
+  [fibonacci: string]: ICardData[];
   'modified fibonacci': ICardData[];
   'power of two': ICardData[];
   'custom/your': ICardData[];
