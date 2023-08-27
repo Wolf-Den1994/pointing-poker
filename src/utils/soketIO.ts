375c8d91006964daa588b7c0d7dcbeae3a2929dd
Denis K <wolf-den@tut.by>
2023-08-27 19:47:06 +0300

    fix: add console

--- src/utils/soketIO.ts
+++ src/utils/soketIO.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 import { BASE_URL } from '../types/types';
 
+console.log('BASE_URL', BASE_URL);
+
 const socket = io(BASE_URL, {
   reconnectionDelay: 5000,
   reconnectionDelayMax: 10000,
